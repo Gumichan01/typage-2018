1e489989288c8c414ac65aefd521a87e6621626d
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-02-24 17:56:13 +0100

    alpha conversion: variable (naive)

--- Conversion.ml
+++ Conversion.ml
@@ -7,21 +7,37 @@
   Author : Luxon JEAN-PIERRE
 *)
 
-Import module ChurchType
+open ChurchType
 
 (*
   A substitution is just a pair <old_name, new_name> that represents {x / y}
   *)
-type substitution = string * string
+type substitution = string * string;;
 
-type subList = substitution list
+type subList = substitution list;;
+
+
+let assoc_if a l =
+  try
+      Some(List.assoc a l)
+  with
+  | _ -> None
 
 (*
-  TODO
+  todo
   It operates the alpha-conversion by taking a church type and a substitution list
   to apply
 
   @note The function fails if a variable capture happened during the alpha-conversion
 *)
-let rec alphaconv : ChurchType.chexpression -> subList -> ChurchType.chexpression =
-  failwith "TODO alpha Conversion"
+let rec alpha_conv e env : ChurchType.chexpression =
+  match e with
+  | ChurchType.Var(s) ->
+    let ns = subs_var s env in
+    ChurchType.Var(ns)
+  | _ -> failwith "TODO the rest"
+
+  and subs_var s env =
+    match assoc_if s env with
+    | None      -> s
+    | Some(sub) -> sub
