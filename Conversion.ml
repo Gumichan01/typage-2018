ae1548b0e1f04e56ce6fa3fce95f0c8360af5ec5
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-02-24 18:12:26 +0100

    conversion: apply

--- Conversion.ml
+++ Conversion.ml
@@ -7,7 +7,7 @@
   Author : Luxon JEAN-PIERRE
 *)
 
-open ChurchType
+open ChurchType;;
 
 (*
   A substitution is just a pair <old_name, new_name> that represents {x / y}
@@ -30,14 +30,26 @@ let assoc_if a l =
 
   @note The function fails if a variable capture happened during the alpha-conversion
 *)
-let rec alpha_conv e env : ChurchType.chexpression =
+let rec alpha_conv e env : chexpression =
   match e with
-  | ChurchType.Var(s) ->
+  | Var(s) ->
     let ns = subs_var s env in
-    ChurchType.Var(ns)
+    Var(ns)
+
+  | Pair(e1, e2) -> Pair((alpha_conv e1 env), (alpha_conv e2 env))
+
+  | Apply(e1, e2) -> Apply((alpha_conv e1 env), (alpha_conv e2 env))
+
   | _ -> failwith "TODO the rest"
 
   and subs_var s env =
     match assoc_if s env with
     | None      -> s
     | Some(sub) -> sub
+;;
+
+
+let che = Pair ( Var("x") , Pair( Var("z"), Var("x") ) ) in
+let env = ("x","y") :: ("z","w") :: [] in
+let res = alpha_conv che env in
+pretty_print_e res; print_endline("");;
