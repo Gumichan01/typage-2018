9b628a5af34192ad1c4bf1a254c2cb085725cbd8
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-04-07 11:36:58 +0200

    expression: dedicated module

---
+++ Expression.mli
@@ -0,0 +1,16 @@
+
+(*
+  IType.ml
+
+  Expression to infer the type of
+
+  Author : Luxon JEAN-PIERRE
+*)
+
+type t =
+    Var of string
+  | Const of string
+  | Pair of t * t
+  | Apply of t * t
+  | Lambda of string * t
+  | Letin of string * t * t
