b9bc17000985e7738c8c35bf5f2f566a4aaadb8c
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-05-05 17:17:15 +0200

    type: interface

--- Type.mli
+++ Type.mli
@@ -5,3 +5,10 @@ type itype =
   | Arrow of itype * itype
   | Tvar of string
 val to_string : itype -> string
+module V :
+  sig
+    type t = string
+    val compare : 'a -> 'a -> int
+    val equal : 'a -> 'a -> bool
+    val create : unit -> itype
+  end
