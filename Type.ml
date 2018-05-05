2cef147040afbe72b83adc1109093af1a3289f01
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-05-05 17:15:45 +0200

    Type: module V

--- Type.ml
+++ Type.ml
@@ -22,3 +22,12 @@ let rec to_string = function
   | Cross(x, y) -> "(" ^(to_string x) ^ " × " ^ (to_string y) ^ ")"
   | Arrow(x, y) -> (to_string x) ^ " → " ^ (to_string y)
   | Tvar(s) -> s
+
+module V = struct
+  type t = string
+  let compare v1 v2 = Pervasives.compare v1 v2
+  let equal v1 v2 = v1 = v2
+  let create = let r = ref 0 in fun () -> incr r; Tvar("α" ^ string_of_int(!r))
+end
+
+(*let free_variable = function*)
