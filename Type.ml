467ec001f8d16d965b77c2befc433c90ba264c47
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-05-06 15:28:03 +0200

    type.ml: refactoring

--- Type.ml
+++ Type.ml
@@ -2,7 +2,7 @@
 (*
   Type.ml
 
-  Type to infer
+  Type system + inference
 
   Author : Luxon JEAN-PIERRE
 *)
@@ -13,26 +13,34 @@ type itype =
   | Int
   | Cross of itype * itype
   | Arrow of itype * itype
-  (* temporay type I am using *)
+  (* temporay type I am using to infer the type *)
   | Tvar of string
 
 let rec to_string = function
   | Int -> "int"
   | Bool -> "bool"
-  | Cross(x, y) -> "(" ^(to_string x) ^ " × " ^ (to_string y) ^ ")"
-  | Arrow(x, y) -> (to_string x) ^ " → " ^ (to_string y)
-  | Tvar(s) -> s
+  | Cross( x, y ) -> "(" ^( to_string x ) ^ " × " ^ ( to_string y ) ^ ")"
+  | Arrow( x, y ) -> ( to_string x ) ^ " → " ^ ( to_string y )
+  | Tvar( s ) -> s
 
 module V = struct
   type t = string
   let compare v1 v2 = Pervasives.compare v1 v2
   let equal v1 v2 = v1 = v2
-  let create = let r = ref 0 in fun () -> incr r; Tvar("α" ^ string_of_int(!r))
+  let create =
+    begin
+      let r = ref 0 in
+      fun () -> incr r; Tvar( "α" ^ string_of_int( !r ) )
+    end
 end
 
+(*
+  Generalization of a type - the famous Gen operator
 
+  @note This implementation is different from how it was defined in class 
+*)
 let rec gen_type = function
-  | Tvar(_) as tv -> tv
+  | Tvar( _ ) as tv -> tv
 
   | Bool | Int -> ( V.create () )
 
@@ -44,12 +52,12 @@ let rec gen_type = function
     let b = ( V.create () ) in
     Arrow( a, b )
 
-  | Arrow( Int, y )  -> Arrow( Int, (gen_type y) )
-  | Arrow( Bool, y ) -> Arrow( Bool, (gen_type y) )
-  | Arrow( x, Int )  -> Arrow( (gen_type x), Int )
-  | Arrow( x, Bool ) -> Arrow( (gen_type x), Bool )
+  | Arrow( Int, y )  -> Arrow( Int, ( gen_type y ) )
+  | Arrow( Bool, y ) -> Arrow( Bool, ( gen_type y ) )
+  | Arrow( x, Int )  -> Arrow( ( gen_type x ), Int )
+  | Arrow( x, Bool ) -> Arrow( ( gen_type x ), Bool )
 
-  | Arrow( x, y ) -> Arrow( (gen_type x), (gen_type y) )
+  | Arrow( x, y ) -> Arrow( ( gen_type x ), ( gen_type y ) )
 
   | Cross( Int, Int )  | Cross( Bool, Bool ) ->
     let a = ( V.create () ) in Cross( a, a )
@@ -59,9 +67,9 @@ let rec gen_type = function
     let b = ( V.create () ) in
     Cross( a, b )
 
-  | Cross( Int, y )  -> Cross( Int, (gen_type y) )
-  | Cross( Bool, y ) -> Cross( Bool, (gen_type y) )
-  | Cross( x, Int )  -> Cross( (gen_type x), Int )
-  | Cross( x, Bool ) -> Cross( (gen_type x), Bool )
+  | Cross( Int, y )  -> Cross( Int, ( gen_type y ) )
+  | Cross( Bool, y ) -> Cross( Bool, ( gen_type y ) )
+  | Cross( x, Int )  -> Cross( ( gen_type x ), Int )
+  | Cross( x, Bool ) -> Cross( ( gen_type x ), Bool )
 
-  | Cross( x, y ) -> Cross( (gen_type x), (gen_type y) )
+  | Cross( x, y ) -> Cross( ( gen_type x ), ( gen_type y ) )
